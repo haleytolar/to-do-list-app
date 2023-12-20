5b68adc946386b8968eafaa4c267145531e718b2
haleytolar <153100456+haleytolar@users.noreply.github.com>
2023-12-19 19:54:28 -0600

    created to do list app
    created todo list app using jquery method in js

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+function newItem(){
+  //add new element
+  let li = $('<li></li>');
+  let inputValue = $('#input').val();
+  li.append(inputValue);
+
+    if(inputValue === ''){
+      alert('Add to list before clicking button.')
+    }else{
+      let list = $('#list');
+      list.append(li);
+    }
+
+  function crossOut(){
+    li.toggleClass('strike');
+  }
+  li.on('dblclick', crossOut);
+
+  let crossOutButton = $('<button></button>');
+  crossOutButton.append(document.createTextNode('X'));
+  li.append(crossOutButton);
+  crossOutButton.addClass('button');
+  crossOutButton.on('click', deleteListItem);
+
+  function deleteListItem(){
+    li.addClass('delete');
+  }
+
+  $('#list').sortable();
+
+}
+    
